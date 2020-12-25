fc5d3999e9575f84041f6c2057a68e900d2d4fcb
Kashish Khullar <Kashish Khullar@LAPTOP-PD58OTN0.(none)>
2020-12-25 17:37:43 +0530

    add image title and buttons

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -8,6 +8,26 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(child: Image.network(imageUrl));
+    return GridTile(
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+    );
   }
 }
