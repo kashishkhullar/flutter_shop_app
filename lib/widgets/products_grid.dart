56bfbc26c90440a4069d61e8c9f316c7971e1bc7
Kashish Khullar <Kashish Khullar@LAPTOP-PD58OTN0.(none)>
2020-12-25 19:23:47 +0530

    add ChangeNotifier, listener to ProductsProvider

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shop_app/models/product.dart';
+import 'package:flutter_shop_app/providers/products_provider.dart';
+import 'package:flutter_shop_app/widgets/product_item.dart';
+import 'package:provider/provider.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsProvider = Provider.of<ProductsProvider>(context);
+    final loadedProducts = productsProvider.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: loadedProducts.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (context, index) {
+        return Container(
+          child: ProductItem(
+            id: loadedProducts[index].id,
+            title: loadedProducts[index].title,
+            imageUrl: loadedProducts[index].imageUrl,
+          ),
+        );
+      },
+    );
+  }
+}
